995d39bcf1e6cf38b9be0c9bc6743c593fbfc2b2
dwang2770 <32362393+dwang2770@users.noreply.github.com>
2017-10-27 09:29:29 -0400

    Update ChatBotWang.java

--- ChatBotKun/src/ChatBotWang.java
+++ ChatBotKun/src/ChatBotWang.java
@@ -1,5 +1,44 @@
 
-public class ChatBotWang 
+public class ChatBotWang
 {
+	int depressionMeter = 0;
+	public String getGreeting()
+	{
+		return "Hi, what is up?";
+	}
+	
+	public String getResponse(String mmwhatchusay)
+	{
+		String response = "";
+		
+		if (mmwhatchusay.length() == 0)
+		{
+			response = "Hello is anyone one there?";
+		}
+		else if (mmwhatchusay.indexOf("yes")!=-1)
+		{
+			response = "Hello how may I help you";
+		}
+		
+	
+		
+	
+	
+	}
+	private boolean ifDie (String mmwhatchusay)
+	{
+		mmwhatchusay = mmwhatchusay.toLowerCase();
+		if (mmwhatchusay.indexOf("I")!=0) {
+			if (mmwhatchusay.indexOf("want to") !=0) {
+				int x = mmwhatchusay.indexOf("die");
+				if (mmwhatchusay.substring(x-1,x).equals(" ") && mmwhatchusay.substring(x+3,x+4).equals(" "))
+				{
+					return true;
+				}
+			}
+		}
+		
+	}
+	
 
 }
