fe3ea1ebf68e00e963aeee9d3c940ded0fe5f180
dwang2770 <32362393+dwang2770@users.noreply.github.com>
2017-10-30 09:28:27 -0400

    Update ChatBotWang.java

--- ChatBotKun/src/ChatBotWang.java
+++ ChatBotKun/src/ChatBotWang.java
@@ -4,7 +4,7 @@ public class ChatBotWang
 	int depressionMeter = 0;
 	public String getGreeting()
 	{
-		return "Hi, what is up?";
+		return "Hello how can I help you?";
 	}
 	
 	public String getResponse(String mmwhatchusay)
@@ -15,21 +15,31 @@ public class ChatBotWang
 		{
 			response = "Hello is anyone one there?";
 		}
-		else if (mmwhatchusay.indexOf("yes")!=-1)
+		else if (ifDie(mmwhatchusay) == true)
 		{
-			response = "Hello how may I help you";
+			depressionMeter -=10;
+			response = "That is not normal you should go get help";
 		}
+		else if 
 		
 	
 		
 	
-	
+		return response;
 	}
 	private boolean ifDie (String mmwhatchusay)
 	{
 		mmwhatchusay = mmwhatchusay.toLowerCase();
-		if (mmwhatchusay.indexOf("I")!=0) {
-			if (mmwhatchusay.indexOf("want to") !=0) {
+		if (mmwhatchusay.indexOf("I")== 0) {
+			if ((mmwhatchusay.indexOf("dont")!= -1 || mmwhatchusay.indexOf("dont")!= -1) && mmwhatchusay.lastIndexOf("die") != 1)
+			{
+				return false;
+			}
+			if (mmwhatchusay.lastIndexOf("die") == 0) 
+			{
+				return true;
+			}
+			if (mmwhatchusay.indexOf("want to") != -1) {
 				int x = mmwhatchusay.indexOf("die");
 				if (mmwhatchusay.substring(x-1,x).equals(" ") && mmwhatchusay.substring(x+3,x+4).equals(" "))
 				{
@@ -37,7 +47,7 @@ public class ChatBotWang
 				}
 			}
 		}
-		
+		return false;
 	}
 	
 
