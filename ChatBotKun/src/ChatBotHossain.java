9d833b34727f7b2f382513935e1aa4265004c505
Rome <Rome@Rome>
2017-10-29 22:10:21 -0400

    we fucked

--- ChatBotKun/src/ChatBotHossain.java
+++ ChatBotKun/src/ChatBotHossain.java
@@ -26,6 +26,11 @@ public class ChatBotHossain
 			wordz = "What's crakalakin homie";
 		}
 		
+		else if (blah.contains("don't want to live") || blah.contains("don't wanna to live"))
+		{
+			wordz = "Now you listen to me and listen good. Killing yourself is the most selfish thing you have could ever do. When you kill yourself you are hurting everyone who loves you to an unbearable extent. "
+					+ "If you are too much of a wimp to live by yourself then live for those who can't bear to see you die." ;
+		}
 				
 		
 				 
