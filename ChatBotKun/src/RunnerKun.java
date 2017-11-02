03edbcc386a50966d333e3d57074df92923fc5cc
dwang2770 <32362393+dwang2770@users.noreply.github.com>
2017-11-02 09:26:16 -0400

    xd

--- ChatBotKun/src/RunnerKun.java
+++ ChatBotKun/src/RunnerKun.java
@@ -1,62 +1,31 @@
 import java.util.Scanner;
 
-public class RunnerKun 
-{ // THIS ISNT THE RUNNER FILE
-	 
-	 /**
-	  * A simple class to run our chatbot teams.
-	  * @author Mr. Levin
-	  * @version September 2017
-	  */
-	
-	 
-	 
-	 	/**
-	 	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
-	 	 
-	 	public static void main(String[] args)
-	 	{
-	 		int YourSelection = 0;
-	 		if (YourSelection == 1)
-	 		{
-	 			ChatBotHossain chatbot1 = new ChatBotHossain();
-	 		
-	 			System.out.println (chatbot1.instahi());
-	 			Scanner in = new Scanner (System.in);
-	 			String statement = in.nextLine();
-	 		
-	 
-	 
-	 			while (!statement.equals("Bye"))
-	 			{
-	 				System.out.println (chatbot1.theybetalkin(statement));
-	 				statement = in.nextLine();
-	 			}
-	 		}
-	 		if (YourSelection == 2)
-	 		{
-	 			ChatBotHossain chatbot2 = new ChatBotHossain();
-	 		
-	 			System.out.println (chatbot2.instahi());
-	 			Scanner in = new Scanner (System.in);
-	 			String statement = in.nextLine();
-	 		
-	 
-	 
-	 			while (!statement.equals("Bye"))
-	 			{
-	 				System.out.println (chatbot2.theybetalkin(statement));
-	 				statement = in.nextLine();
-	 			}
-	 		}
-	 	
-	 	
-	 	
-	 	}
-	 
+/**
+ * A simple class to run our chatbot teams.
+ * @author Mr. Levin
+ * @version September 2017
+ */
+public class RunnerKun
+{
+
+	/**
+	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
+	 */
+	public static void main(String[] args)
+	{
+		ChatBotWang chatbot1 = new ChatBotWang();
+		
+		System.out.println (chatbot1.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String mmwhatchusay = in.nextLine();
+		
+
+
+		while (!mmwhatchusay.equals("Bye"))
+		{
+			System.out.println (chatbot1.getResponse(mmwhatchusay));
+			mmwhatchusay = in.nextLine();
+		}
+	}
+
 }
-	
-
-
-	
-*/
